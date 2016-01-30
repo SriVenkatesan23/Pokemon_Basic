c11c593a2e90fae6e49e1ea413bf156f3f4f078b
SriVenkatesan23 <sri.venkatesan23@gmail.com>
2016-01-30 18:50:41 -0500

    Create Attacks.java

---
+++ Attacks.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Attacks {
+
+	private String type;
+	private double regD;
+	private String name;
+	
+
+	public Attacks(String nName, String newType, double newRegD){
+		name=nName;
+		type=newType;
+		regD = newRegD;
+		
+	}
+	
+	public String getType(){
+		return type;
+		
+	}	
+	
+	public double getDamage(){
+		
+		return regD;	
+		
+	}
+	
+	
+	public String getName(){
+		return name;
+		
+	}
+	public int hit(){
+		int check=new Random().nextInt(99);
+		if(check<5){
+			return 0;
+		}
+		else{
+			return 1;
+		}
+	}
+	
+	
+	
+}
